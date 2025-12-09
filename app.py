32380c0ad0f22c23ec80a26c0e5e5611f3d0e6f8
lxinyi030201 <lxinyi030201@outlook.com>
2025-12-09 03:32:36 +0000

    1

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    r=request.form.get("q")
+    r = request.form.get("q")
     return(render_template("index.html"))
 
 @app.route("/main", methods=["GET","POST"])
 def main():
-        return(render_template("main.html"))
+    return(render_template("main.html"))
 
 @app.route("/dbs", methods=["GET","POST"])
 def dbs():
@@ -17,12 +18,26 @@ def dbs():
 
 @app.route("/DbsPrediction", methods=["GET","POST"])
 def DbsPrediction():
-    r=request.form.get("q")
-    print(r)
+    q = float(request.form.get("q"))
+    model = joblib.load("dbs.jl")
+    r = model.predict([[q]])
+    return(render_template("DbsPrediction.html",r=r[0][0]))
 
 
-    return(render_template("index.html"))
+@app.route("/credit", methods=["GET","POST"])
+def credit():
+    return(render_template("credit.html"))
+
+@app.route("/creditPrediction", methods=["GET","POST"])
+def creditPrediction():
+    q = float(request.form.get("q"))
+    model = joblib.load("/workspaces/LIUXINYI/credit_model.pkl")
+    r = model.predict([[q]])
+    if r == 1:
+        r = "Aproved"
+    else:
+        r = "Not Approved"
+    return(render_template("creditPrediction.html",r=r))
 
 if __name__ == "__main__":
     app.run()
-
